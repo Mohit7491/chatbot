919f0a4bb82bf9c84c107854beb9a2b502426893
Mokshi Jain <mohiit.mohitjain9@gmail.com>
2026-04-18 09:43:16 +0530

    history+env variablecreated

--- chatbot.py
+++ chatbot.py
@@ -1,30 +1,47 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+api_key = os.getenv("OPENROUTER_API_KEY")
 import requests
-
-# 👉 user input
-user_input = input("Ask something: ")
-
 url = "https://openrouter.ai/api/v1/chat/completions"
 
 headers = {
-    "Authorization": "Bearer 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",
+    "Authorization": f"Bearer {api_key}",
     "Content-Type": "application/json"
 }
 
-data = {
+
+messages = [
+    {"role": "system", "content": "You are a strict AI mentor. Give clear, step-by-step answers."}
+]
+# 👉 user input
+while True:
+    user_input = input("Ask something: ")
+    if user_input.lower() == "exit":
+      break
+    messages.append({"role": "user", "content": user_input})
+
+    data = {
     "model": "openai/gpt-3.5-turbo",
-    "messages": [
-        {"role": "user", "content": user_input}
-    ]
-}
+    "messages": messages
+    }
+    
 
-response = requests.post(url, headers=headers, json=data)
+    response = requests.post(url, headers=headers, json=data)
 
-result = response.json()
-
-answer = result["choices"][0]["message"]["content"]
-
-# 👇 file save system
-with open("conversation.txt", "a") as f:
-    f.write("User: " + user_input + "\n")
-    f.write("AI: " + answer + "\n\n")
-print("\nAI:", answer)
+    result = response.json()
+    
+     # 🔐 safety check (important)
+    if "choices" not in result:
+        print("Error:", result)
+        continue
+    answer = result["choices"][0]["message"]["content"]
+    messages.append({"role": "assistant", "content": answer})
+    
+    print("\nAI:", answer)
+    # 👇 file save system
+    with open("conversation.txt", "a") as f:
+      f.write("User: " + user_input + "\n")
+      f.write("AI: " + answer + "\n\n")
